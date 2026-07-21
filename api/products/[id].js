7db3f73676812db740b0c438fd8be8e326603b28
SyabAhmad <syabblogger@gmail.com>
2026-07-21 16:29:59 +0300

    debug: test connectDB import

--- api/products/[id].js
+++ api/products/[id].js
@@ -1,4 +1,11 @@
+import connectDB from '../../_lib/db.js';
+
 export default async function handler(req, res) {
-  const { id } = req.query;
-  return res.json({ id, method: req.method });
+  try {
+    await connectDB();
+    const { id } = req.query;
+    return res.json({ id, method: req.method, connected: true });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 }
